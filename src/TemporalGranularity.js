64ca120c1112d1c97bcbaaac2882104a11bfd0ea
Carlos Zimmerle <cezl@cin.ufpe.br>
2018-07-15 21:32:33 -0300

    deletion of TemporalGranularity

--- src/TemporalGranularity.js
+++
@@ -1,9 +0,0 @@
-var TemporalGranularity;
-(function (TemporalGranularity) {
-    TemporalGranularity[TemporalGranularity["microsecond"] = 0] = "microsecond";
-    TemporalGranularity[TemporalGranularity["millisecond"] = 1] = "millisecond";
-    TemporalGranularity[TemporalGranularity["second"] = 2] = "second";
-    TemporalGranularity[TemporalGranularity["minute"] = 3] = "minute";
-    TemporalGranularity[TemporalGranularity["hour"] = 4] = "hour";
-    TemporalGranularity[TemporalGranularity["day"] = 5] = "day";
-})(TemporalGranularity || (TemporalGranularity = {}));
