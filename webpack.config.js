ecbef5a3b2b4eea2bbfb7c13569d64ab105e4db3
Carlos Zimmerle <cezl@cin.ufpe.br>
2019-04-03 15:52:24 -0300

    feat: add babel to transpile the build to es5

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = (env, argv) => { 
+module.exports = (env, argv) => {
   const mode = argv.mode || 'production';
   return {
     entry: './lib',
@@ -9,6 +9,21 @@ module.exports = (env, argv) => {
       path: path.resolve('dist'),
       filename: 'cep.js',
       library: 'cepjs'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread']
+            }
+          }
+        }
+      ]
     }
   }
 }
