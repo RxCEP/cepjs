449c159f0d2ec2e650384bfbb2de34ecd673ddf8
Carlos Zimmerle <cezl@cin.ufpe.br>
2019-04-16 22:51:27 -0300

    refactor: simplify webpack bundle generation

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,36 @@
 const path = require('path');
 
-module.exports = (env, argv) => {
-  const mode = argv.mode || 'production';
-  return {
-    entry: './lib',
-    mode: mode,
-    output: {
-      path: path.resolve('dist'),
-      filename: 'cep.js',
-      library: 'cepjs'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread']
-            }
+const config = {
+  entry: './lib',
+  mode: 'production',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'cep.min.js',
+    library: 'cepjs'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
           }
         }
-      ]
-    }
+      }
+    ]
   }
+};
+
+module.exports = (env, argv) => {
+  
+  if(argv.mode === 'development'){
+    config.mode = argv.mode;
+    config.output.filename = 'cep.js';
+  }
+  
+  return config;
 }
