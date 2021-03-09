9e17a34cfe9cf46e44fdbca8e628f1a86ec852ab
Carlos Zimmerle <cezl@cin.ufpe.br>
2021-03-09 18:04:28 -0300

    chore: add comments to aid readability

--- packages/cepjs-core/lib/reactive-libs/common/logical.js
+++ packages/cepjs-core/lib/reactive-libs/common/logical.js
@@ -1,6 +1,19 @@
 const _ = require('lodash/fp');
+
 const { compareEvtType } = require('../helperFunctions');
 
+// counts the elements of a set/array according to the given argument
+// identity represents a function that returns the passed value
+// this function is used to count the occurrences of event types
+const countList = _.countBy(_.identity);
+
+/**
+ * 
+ * @param {string[]} evtTypeList - original event type list.
+ * @param {Object} evtTypeListCounted - an object containing the total of
+ * event types in the event type list. This object is the result of the countList function.
+ * @return {EventType[][]} a set of arrays, each grouped by the occurrences listed on the event type list.
+ */
 const separateEvtsByType = _.curry((evtTypeList, evtTypeListCounted, elems) => {
   return _.uniq(evtTypeList).reduce((acc, evtType) => {
     acc.push(evtTypeListCounted[evtType] === 1 ?
@@ -10,8 +23,8 @@ const separateEvtsByType = _.curry((evtTypeList, evtTypeListCounted, elems) => {
   }, []);
 });
 
-const countList = _.countBy(_.identity);
-
+// calculates the cartesian product of a set of informed arrays
+// function taken from https://gist.github.com/ChrisJefferson/cb8db2a4c67a9506c56c
 const cartesianProduct = (...rest) =>
   _.reduce((a, b) =>
     _.flatMap(x =>
